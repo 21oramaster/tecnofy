fb0a2ed855d28eb0cae07cd446943de160bddbf8
21oramaster <100649519+21oramaster@users.noreply.github.com>
2024-03-23 22:17:23 -0300

    Update iportainer.sh

--- iportainer.sh
+++ iportainer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker pull portainer/portainer-ce:latest
-sudo docker run -d -p 9100:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+docker pull portainer/portainer-ce:latest
+docker run -d -p 9100:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
