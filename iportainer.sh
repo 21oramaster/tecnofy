487abce0dfd06a908fc1143b3546b650916c36f6
21oramaster <100649519+21oramaster@users.noreply.github.com>
2024-03-23 22:16:24 -0300

    Create iportainer.sh

---
+++ iportainer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker pull portainer/portainer-ce:latest
+sudo docker run -d -p 9100:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
